7bf546aadeb49f90e786b554ca8403370c1f7201
Duruwilly <duruprincewilluzochukwu@gmail.com>
2022-09-11 01:22:28 +0100

    homepage commit

--- foodweb/src/pages/Home.js
+++ foodweb/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import AboutUs from '../components/AboutUs'
+import Footer from '../components/Footer'
 import HalfMenuList from '../components/HalfMenuList'
 import Heroe from '../components/Heroe'
+import Newsletter from '../components/Newsletter'
 import Reviews from '../components/Reviews'
 import SpecialDish from '../components/SpecialDish'
 
@@ -13,6 +15,8 @@ const Home = () => {
      <SpecialDish />
      <HalfMenuList />
      <Reviews />
+     <Newsletter />
+     <Footer />
     </>
   )
 }
